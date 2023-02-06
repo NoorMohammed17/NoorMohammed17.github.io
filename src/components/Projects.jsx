5e66e240f786d15de1bbd614be2aff6661bf3583
NoorMohammed17 <noorshaik1798@gmail.com>
2023-02-06 23:06:17 +0530

    projects modified

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,76 +1,92 @@
-import React from 'react'
-import licious from "./Pictures/licious.PNG"
-import himalaya from "./Pictures/himalaya.PNG"
-import {  Image,  Link, Flex ,Button} from "@chakra-ui/react";
+import React from "react";
+import licious from "./Pictures/licious.PNG";
+import himalaya from "./Pictures/himalaya.PNG";
+import { Image, Link, Flex, Button } from "@chakra-ui/react";
 
 const Projects = () => {
+  const licious_Url = "https://guileless-kleicha-1e91f4.netlify.app/";
+  // const nordstrom_Url = "";
+  const himalaya_Url = "https://friendly-pudding-a5d011.netlify.app/homepage/";
+  // const github_himalaya_Url="https://github.com/btcpatil/handy-zipper-9467";
 
-     const licious_Url ="https://guileless-kleicha-1e91f4.netlify.app/";
-    // const nordstrom_Url = "";
-    const himalaya_Url="https://friendly-pudding-a5d011.netlify.app/homepage/";
-    // const github_himalaya_Url="https://github.com/btcpatil/handy-zipper-9467";
-    
   return (
-    <div  id="projects">
-      <h1 style={{textAlign:"center"}}> My Projects</h1>
+    <div id="projects">
+      <h1 style={{ textAlign: "center" }}> My Projects</h1>
 
-      <div style={{display:"flex", marginBottom:"30px"}}>
-        <div style={{ width:"65%", marginRight:"10px"}}>
-        <img src={licious} alt="licious"  />
+      <div style={{ display: "flex", marginBottom: "30px" }}>
+        <div style={{ width: "65%", marginRight: "10px" , border:"1px solid red"}}>
+          <img src={licious} alt="licious"  width="100%" />
         </div>
         <div>
-        <h1>Licious Clone</h1>
-        <Flex id="tech">
-          
-        <Image w={"50px"} src="https://chiranjeev-thapliyal.vercel.app/svg/html-5.svg"/>
-        <Image w={"50px"} src="https://chiranjeev-thapliyal.vercel.app/svg/javascript.svg"/>
-        <Image w={"50px"} src="https://chiranjeev-thapliyal.vercel.app/svg/css3.svg"/>
-       
-        </Flex>
-        <Flex>
-        <Link href="https://github.com/PratyayChakraborty/obscene-pest-6443" isExternal><Image w={"50px"} src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"></Image></Link>
-        <Link href={licious_Url} isExternal>
-          <Button colorScheme='purple'>Visit Site</Button>
-        </Link>
-        </Flex>
+          <h1>Licious Clone</h1>
+          <Flex id="tech">
+            <Image
+              w={"50px"}
+              src="https://chiranjeev-thapliyal.vercel.app/svg/html-5.svg"
+            />
+            <Image
+              w={"50px"}
+              src="https://chiranjeev-thapliyal.vercel.app/svg/javascript.svg"
+            />
+            <Image
+              w={"50px"}
+              src="https://chiranjeev-thapliyal.vercel.app/svg/css3.svg"
+            />
+          </Flex>
+          <Flex>
+            <Link
+              href="https://github.com/PratyayChakraborty/obscene-pest-6443"
+              isExternal
+            >
+              <Image
+                w={"50px"}
+                src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+              ></Image>
+            </Link>
+            <Link href={licious_Url} isExternal>
+              <Button colorScheme="purple">Visit Site</Button>
+            </Link>
+          </Flex>
         </div>
-        
-        
-        
-
       </div>
-      <div style={{display:"flex"}}>
-        <div style={{ width:"65%", marginRight:"10px"}}>
-          <img src={himalaya} alt="himalaya" />
-          </div>
+      <div style={{ display: "flex" }}>
+        <div style={{ width: "65%", marginRight: "10px" }}>
+          <img src={himalaya} alt="himalaya"  width="100%"/>
+        </div>
         <div>
           <h1>Himalaya Clone</h1>
           <Flex>
-          
-        <Image w={"50px"} src="https://chiranjeev-thapliyal.vercel.app/svg/html-5.svg"/>
-        <Image w={"50px"} src="https://chiranjeev-thapliyal.vercel.app/svg/javascript.svg"/>
-        <Image w={"50px"} src="https://chiranjeev-thapliyal.vercel.app/svg/css3.svg"/>
-       
-        
-        </Flex>
-        <Flex>
-        <Link href="https://github.com/btcpatil/handy-zipper-9467" isExternal><Image w={"50px"} src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"></Image></Link>
-        <Link href={himalaya_Url} isExternal>
-          <Button colorScheme='purple'>Visit Site</Button>
-        </Link>
-        </Flex>
-          
+            <Image
+              w={"50px"}
+              src="https://chiranjeev-thapliyal.vercel.app/svg/html-5.svg"
+            />
+            <Image
+              w={"50px"}
+              src="https://chiranjeev-thapliyal.vercel.app/svg/javascript.svg"
+            />
+            <Image
+              w={"50px"}
+              src="https://chiranjeev-thapliyal.vercel.app/svg/css3.svg"
+            />
+          </Flex>
+          <Flex>
+            <Link
+              href="https://github.com/btcpatil/handy-zipper-9467"
+              isExternal
+            >
+              <Image
+                w={"50px"}
+                src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+              ></Image>
+            </Link>
+            <Link href={himalaya_Url} isExternal>
+              <Button colorScheme="purple">Visit Site</Button>
+            </Link>
+          </Flex>
         </div>
-
-        
-
-        
-
       </div>
-
-      
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
